2f009034383766cbd80ab573878435cad5ef25c7
Troy McCall <troyxmccall@gmail.com>
2023-10-05 14:51:28 -0400

    more build changes

--- buildx-local.sh
+++ buildx-local.sh
@@ -24,7 +24,7 @@ cleanup() {
 }
 
 # Function to check when the image was last created locally
-was_created_last_hour() {
+was_created_last_day() {
     local image="$1"
 
     # Get the image creation time using docker inspect
@@ -33,19 +33,20 @@ was_created_last_hour() {
     # Convert the timestamp to seconds
     local created_time=$($DATE_CMD --date="$timestamp" +%s)
     local current_time=$($DATE_CMD +%s)
-    local one_hour_in_seconds=3600
+    local one_day_in_seconds=86400
 
     # Calculate the difference in time
     local time_diff=$((current_time - created_time))
 
-    # If the time difference is less than an hour (3600 seconds), return 0 (true)
-    if [ "$time_diff" -lt "$one_hour_in_seconds" ]; then
+    # If the time difference is less than a day (86400 seconds), return 0 (true)
+    if [ "$time_diff" -lt "$one_day_in_seconds" ]; then
         return 0
     else
         return 1
     fi
 }
 
+trap 'echo "Error on line $LINENO"' ERR
 trap cleanup SIGINT SIGTERM
 
 docker context use default || true
@@ -63,9 +64,12 @@ docker buildx create --use builder
 TYPES=("cli" "fpm")
 PHP_VERSIONS=("7.1" "7.2" "7.4" "8.0" "8.1" "8.2")
 
-for TYPE in "${TYPES[@]}"; do
-    for VERSION in "${PHP_VERSIONS[@]}"; do
+# First, loop through PHP versions
+for VERSION in "${PHP_VERSIONS[@]}"; do
+    # Then, loop through the types for each version
+    for TYPE in "${TYPES[@]}"; do
         DIR="${TYPE}/${VERSION}"
+        echo "Processing version: ${VERSION}, type: ${TYPE}"
         if [[ -f "${DIR}/.env" ]]; then
             # Source environment variables from the .env file
             set -a
@@ -74,13 +78,38 @@ for TYPE in "${TYPES[@]}"; do
 
             TAG_NAME="mxmd/php:${PHP_VERSION}-${TYPE}"
 
-            # Try to pull the image. If not available locally, this will ensure you have the latest metadata.
-            docker pull "mxmd/php:${PHP_VERSION}-${TYPE}" || true
+            # Disable the 'exit on error' behavior
+            set +e
 
-            if was_created_last_hour "mxmd/php:${PHP_VERSION}-${TYPE}"; then
-                echo "Image mxmd/php:${PHP_VERSION}-${TYPE} was created within the last hour. Skipping build."
+            # Attempt to pull the image and capture the output/error
+            PULL_OUTPUT=$(docker pull "mxmd/php:${PHP_VERSION}-${TYPE}" 2>&1)
+            PULL_STATUS=$?
+
+            # Print the output for debugging
+            echo "Pull output for mxmd/php:${PHP_VERSION}-${TYPE}:"
+            echo "--------------------------------------"
+            echo "$PULL_OUTPUT"
+            echo "--------------------------------------"
+
+            # Check for "No such object" error in the pull output
+            if [[ $PULL_OUTPUT == *"Error: No such object:"* ]]; then
+                echo "Warning: Image mxmd/php:${PHP_VERSION}-${TYPE} not found."
+            # Check for "manifest unknown" error in the pull output
+            elif [[ $PULL_OUTPUT == *"manifest unknown: manifest unknown"* ]]; then
+                echo "Warning: Image mxmd/php:${PHP_VERSION}-${TYPE} manifest unknown."
+            # Check for other errors based on the pull command exit status
+            elif [[ $PULL_STATUS -ne 0 ]]; then
+                echo "Error pulling mxmd/php:${PHP_VERSION}-${TYPE}. Exiting."
+                exit 1
+            else
+              if was_created_last_day "mxmd/php:${PHP_VERSION}-${TYPE}"; then
+                echo "Image mxmd/php:${PHP_VERSION}-${TYPE} was created within the last day. Skipping build."
                 continue
+              fi
             fi
+
+            # Exit immediately if a command exits with a non-zero status
+            set -e
 
             docker buildx build \
               --push \
