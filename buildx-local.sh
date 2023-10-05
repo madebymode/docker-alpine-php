0fc421caaea78d3faea61d87363b4fbddeaeaa5c
Troy McCall <troyxmccall@gmail.com>
2023-10-05 13:37:48 -0400

    update build script and versions

--- buildx-local.sh
+++ buildx-local.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+DATE_CMD=date
+if [[ "$(uname)" == "Darwin" ]]; then
+    # If on macOS, check if gdate is available
+    if command -v gdate > /dev/null; then
+        DATE_CMD=gdate
+    else
+        echo "Error: GNU date (gdate) is not installed. Install it using Homebrew (brew install coreutils)."
+        exit 1
+    fi
+fi
+
+
 # Exit immediately if a command exits with a non-zero status
 set -e
 
@@ -9,6 +21,29 @@ cleanup() {
     docker context use default
     docker context rm builder
     exit
+}
+
+# Function to check when the image was last created locally
+was_created_last_hour() {
+    local image="$1"
+
+    # Get the image creation time using docker inspect
+    local timestamp=$(docker inspect --format '{{.Created}}' "$image")
+
+    # Convert the timestamp to seconds
+    local created_time=$($DATE_CMD --date="$timestamp" +%s)
+    local current_time=$($DATE_CMD +%s)
+    local one_hour_in_seconds=3600
+
+    # Calculate the difference in time
+    local time_diff=$((current_time - created_time))
+
+    # If the time difference is less than an hour (3600 seconds), return 0 (true)
+    if [ "$time_diff" -lt "$one_hour_in_seconds" ]; then
+        return 0
+    else
+        return 1
+    fi
 }
 
 trap cleanup SIGINT SIGTERM
@@ -39,6 +74,14 @@ for TYPE in "${TYPES[@]}"; do
 
             TAG_NAME="mxmd/php:${PHP_VERSION}-${TYPE}"
 
+            # Try to pull the image. If not available locally, this will ensure you have the latest metadata.
+            docker pull "mxmd/php:${PHP_VERSION}-${TYPE}" || true
+
+            if was_created_last_hour "mxmd/php:${PHP_VERSION}-${TYPE}"; then
+                echo "Image mxmd/php:${PHP_VERSION}-${TYPE} was created within the last hour. Skipping build."
+                continue
+            fi
+
             docker buildx build \
               --push \
               --platform linux/amd64,linux/arm64 \
@@ -47,7 +90,7 @@ for TYPE in "${TYPES[@]}"; do
               --build-arg ALPINE_VERSION="${ALPINE_VERSION}" \
               --build-arg ALPINE_IMAGE="alpine:${ALPINE_VERSION}" \
               --file "${DIR}/Dockerfile" \
-              cli/
+              $TYPE/
 
         fi
     done
